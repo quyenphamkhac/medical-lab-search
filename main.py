047a2287417c2b554d68167b288a6389aaeb6ee0
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2022-12-22 12:39:01 +0700

    feat: documentai quickstart

--- main.py
+++ main.py
@@ -1,119 +1,51 @@
-from typing import Sequence
+import os
+
+from google.api_core.client_options import ClientOptions
 from google.cloud import documentai
-import json
+
+# TODO(developer): Uncomment these variables before running the sample.
+# project_id = 'YOUR_PROJECT_ID'
+# location = 'YOUR_PROCESSOR_LOCATION' # Format is 'us' or 'eu'
+# processor_id = 'YOUR_PROCESSOR_ID' #  Create processor before running sample
+# file_path = '/path/to/local/pdf'
+# mime_type = 'application/pdf' # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './service_account/medical_lab_sa.json'
 
 
-def process_document_ocr_sample() -> None:
-    f = open('response.json')
-    data = json.loads(f. read())
-    document = data['document']
-    text = document['text']
-    # print(f"Full document text: {text}\n")
-    # print(f"There are {len(document['pages'])} page(s) in this document.\n")
+def quickstart(
+    project_id: str, location: str, processor_id: str, file_path: str, mime_type: str
+):
+    # You must set the api_endpoint if you use a location other than 'us', e.g.:
+    opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
 
-    for page in document['pages']:
-        # print(page['detectedLanguages'])
-        # print_page_dimensions(page['dimension'])
-        # print_detected_langauges(page['detectedLanguages'])
-        # print_paragraphs(page['paragraphs'], text)
-        # print_blocks(page['blocks'], text)
-        print_lines(page['lines'], text)
-        # print_tokens(page['tokens'], text)
+    client = documentai.DocumentProcessorServiceClient(client_options=opts)
 
+    # The full resource name of the processor, e.g.:
+    # projects/project_id/locations/location/processor/processor_id
+    name = client.processor_path(project_id, location, processor_id)
 
-def print_page_dimensions(dimension) -> None:
-    print(f"    Width: {str(dimension['width'])}")
-    print(f"    Height: {str(dimension['height'])}")
+    # Read the file into memory
+    with open(file_path, "rb") as image:
+        image_content = image.read()
 
+    # Load Binary Data into Document AI RawDocument Object
+    raw_document = documentai.RawDocument(
+        content=image_content, mime_type=mime_type)
 
-def print_detected_langauges(
-    detected_languages,
-) -> None:
-    print("    Detected languages:")
-    for lang in detected_languages:
-        code = lang['languageCode']
-        print(f"        {code} ({lang['confidence']:.1%} confidence)")
+    # Configure the process request
+    request = documentai.ProcessRequest(name=name, raw_document=raw_document)
 
+    result = client.process_document(request=request)
 
-def print_paragraphs(
-    paragraphs, text: str
-) -> None:
-    print(f"    {len(paragraphs)} paragraphs detected:")
-    first_paragraph_text = layout_to_text(paragraphs[0]['layout'], text)
-    print(f"        First paragraph text: {repr(first_paragraph_text)}")
-    last_paragraph_text = layout_to_text(paragraphs[-1]['layout'], text)
-    print(f"        Last paragraph text: {repr(last_paragraph_text)}")
+    # For a full list of Document object attributes, please reference this page:
+    # https://cloud.google.com/python/docs/reference/documentai/latest/google.cloud.documentai_v1.types.Document
+    document = result.document
+
+    # Read the text recognition output from the processor
+    print("The document contains the following text:")
+    print(document.text)
 
 
-def print_blocks(blocks, text: str) -> None:
-    print(f"    {len(blocks)} blocks detected:")
-    first_block_text = layout_to_text(blocks[0]['layout'], text)
-    print(f"        First text block: {repr(first_block_text)}")
-    last_block_text = layout_to_text(blocks[-1]['layout'], text)
-    print(f"        Last text block: {repr(last_block_text)}")
-
-
-def print_lines(lines, text: str) -> None:
-    print(f"    {len(lines)} lines detected:")
-    for line in lines:
-        line_text = layout_to_text(line['layout'], text)
-        print(f"        Line text: {repr(line_text)}")
-    # first_line_text = layout_to_text(lines[0]['layout'], text)
-    # print(f"        First line text: {repr(first_line_text)}")
-    # last_line_text = layout_to_text(lines[-1]['layout'], text)
-    # print(f"        Last line text: {repr(last_line_text)}")
-
-
-def print_tokens(tokens, text: str) -> None:
-    print(f"    {len(tokens)} tokens detected:")
-    for token in tokens:
-        token_text = layout_to_text(token['layout'], text)
-        # token_text_break_type = ''
-        # try:
-        #     token_text_break_type = token['detectedBreak']['type']
-        # except:
-        #     print(token)
-        print(f"        First token text: {repr(token_text)}")
-        # print(f"        First token break type: {repr(token_text_break_type)}")
-    # first_token_text = layout_to_text(tokens[0]['layout'], text)
-    # first_token_break_type = tokens[0]['detected_break']['type_']['name']
-    # print(f"        First token text: {repr(first_token_text)}")
-    # print(f"        First token break type: {repr(first_token_break_type)}")
-    # last_token_text = layout_to_text(tokens[-1]['layout'], text)
-    # last_token_break_type = tokens[-1]['detected_break']['type_']['name']
-    # print(f"        Last token text: {repr(last_token_text)}")
-    # print(f"        Last token break type: {repr(last_token_break_type)}")
-
-
-# def print_image_quality_scores(
-#     image_quality_scores,
-# ) -> None:
-#     print(f"    Quality score: {image_quality_scores['quality_score']:.1%}")
-#     print("    Detected defects:")
-
-#     for detected_defect in image_quality_scores.detected_defects:
-#         print(
-#             f"        {detected_defect['type_']}: {detected_defect['confidence']:.1%}")
-
-
-def layout_to_text(layout, text: str) -> str:
-    """
-    Document AI identifies text in different parts of the document by their
-    offsets in the entirety of the document's text. This function converts
-    offsets to a string.
-    """
-    response = ""
-    # If a text segment spans several lines, it will
-    # be stored in different text segments.
-    for segment in layout['textAnchor']['textSegments']:
-        start_index = 0
-        try:
-            start_index = int(segment['startIndex'])
-        except:
-            print(segment)
-        end_index = int(segment['endIndex'])
-        response += text[start_index:end_index]
-    return response
-
-
-process_document_ocr_sample()
+quickstart(project_id="conductive-bank-365208", location="us", processor_id="34e550bda24b8927",
+           file_path="./data/lab_result_1.png", mime_type="image/png")
