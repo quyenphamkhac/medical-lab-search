de10b6e01ae893dbe35fe3e201982a5c4922a257
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2023-01-03 14:22:06 +0700

    feat: final version

--- lab_helpers.py
+++ lab_helpers.py
@@ -1,49 +1,5 @@
-from difflib import get_close_matches
-from thefuzz import fuzz, process
-from typing import List, Tuple
-
-LABS = ['WCB', 'BẠCH CẦU', '%NEU', 'NEUT%', '%LYM', 'LYMPH%',
-        '%MONO', 'MONO%', '%EOS', 'EO%', '%BASO', 'BASO%', 'NEU',
-        'NEUT#', 'LYM', 'LYMPH#', 'MONO', 'MONO#', 'EOS', 'EO#', 'BASO',
-        'BASO#', 'RBC', 'HỒNG CẦU', 'HGB', 'HCT', 'MCV', 'MCH', 'MCHC',
-        'RDW', 'PLT', 'MPV', 'PDW', 'PCT']
-
-LAB_NAMES = {
-    'WCB': 'WCB',
-    'BẠCH CẦU': 'Bạch cầu',
-    '%NEU': '%NEU',
-    'NEUT%': 'NEUT%',
-    '%LYM': '%LYM',
-    'LYMPH%': 'LYMPH%',
-    '%MONO': '%MONO',
-    'MONO%': 'MONO%',
-    '%EOS': '%EOS',
-    'EO%': 'EO%',
-    '%BASO': '%BASO',
-    'BASO%': 'BASO%',
-    'NEU': 'NEU',
-    'NEUT#': 'NEUT#',
-    'LYM': 'LYM',
-    'LYMPH#': 'LYMPH#',
-    'MONO': 'MONO',
-    'MONO#': 'MONO#',
-    'EOS': 'EOS',
-    'EO#': 'EO#',
-    'BASO': 'BASO',
-    'BASO#': 'BASO#',
-    'RBC': 'RBC',
-    'HỒNG CẦU': 'Hồng cầu',
-    'HGB': 'HGB',
-    'HCT': 'HCT',
-    'MCV': 'MCV',
-    'MCH': 'MCH',
-    'MCHC': 'MCHC',
-    'RDW': 'RDW',
-    'PLT': 'PLT',
-    'MPV': 'MPV',
-    'PDW': 'PDW',
-    'PCT': 'PCT'
-}
+from thefuzz import process
+from typing import List
 
 samples = [
     "Yêu cầu xét nghiệm",
@@ -182,23 +138,6 @@ master_data = {
         "other_names": ["RBC", "Số lượng hồng cầu", "red blood cell"]
     }
 }
-
-
-def find_best_match_lab(text: str) -> str:
-    best_match_lab_name = ''
-    matches = get_close_matches(text.lower(), LABS, n=1, cutoff=0.6)
-    if len(matches) > 0:
-        best_match_lab = matches.pop()
-        best_match_lab_name = LAB_NAMES[best_match_lab]
-    return best_match_lab_name
-
-
-def correct_value(value: float, unit: str) -> float:
-    return value
-
-
-def preprocessing_lines(lines: List[str]) -> List[str]:
-    return [x.lower() for x in lines]
 
 
 def find_best_match(keyword: str, choices: List[str]):
